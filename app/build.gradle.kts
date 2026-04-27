fea581fb267347a8d598c4e8df33b5195a9e15f9
KYAHDJ <benitezdavid663@gmail.com>
2026-04-27 11:42:03 +0800

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.dosevia.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 3
         versionName = "1"
     }
 
